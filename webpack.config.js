3d69b4a9274ba20a0d647eefff63647aad52c8df
Timur <makarov-timyr@yandex.ru>
2021-03-22 15:02:10 +0300

    tuned project config

---
+++ webpack.config.js
@@ -0,0 +1,134 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimazeCssAssetPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
+
+const cssLoaders = (extra) => {
+    const loaders = [
+        {
+            loader: MiniCssExtractPlugin.loader
+        },
+        `css-loader`
+    ] 
+
+    if (extra) loaders.push(extra)
+    return loaders
+}
+
+const babelLoaders = () => {
+    const loaders =
+     {
+        presets: [
+            '@babel/preset-env'
+        ],
+        plugins: [
+            '@babel/plugin-proposal-class-properties'
+        ]
+    }
+
+    if (isDev) {
+        loaders.push('eslint-loader')
+    }
+
+    return loaders
+}
+
+const optimaze = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+
+    if (isProd) {
+        config.minimizer = [
+            new OptimazeCssAssetPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
+
+    return config
+}
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: {
+        main: './index.js',
+    },
+    output: {
+        filename: filename('js'),
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.js', '.json', '.png', '.css'],
+        alias: {
+            "@": path.resolve(__dirname, 'src'),
+            "@core": path.resolve(__dirname, 'src/core')
+        }
+    },
+    devtool: isDev ? 'source-map' : '',
+    devServer: {
+        port: 3000,
+        hot: isDev
+    },
+    optimization: optimaze(),
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            minify: {
+                collapseWhitespace: isProd,
+                removeComments: isProd
+            }
+        }),
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [{
+                from: path.resolve(__dirname, 'src/icon.png'),
+                to: path.resolve(__dirname, 'dist')
+            }]
+        }),
+        new MiniCssExtractPlugin({
+            filename: filename('css')
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: cssLoaders()
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: cssLoaders('sass-loader')
+            },
+            {
+                test: /\.(png|jpg|svg|jpeg)$/i,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/i,
+                use: ['file-loader']
+            },
+            {
+                test: /\.csv$/i,
+                use: ['csv-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: babelLoaders()
+            }
+        ]
+    }
+
+}
